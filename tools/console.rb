ea680053db934304b907eccb3f3b6d93367623a0
+ <+>
2022-10-31 16:33:03 +0300

    Debugging

--- tools/console.rb
+++ tools/console.rb
@@ -7,8 +7,8 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 # Author
-author1 = Author.new( "kendi")
-author2= Author.new( "martha")
+kendi = Author.new("kendi")
+martha = Author.new("martha")
 
 # Magazine
 people = Magazine.new("People", "Entertainment")
@@ -18,14 +18,14 @@ people_daily = Magazine.new("People Daily", "Entertainment")
 
 # Article
 
-art1=Article.new(author1, people, "How to lose weight fast")
-art2=Article.new(author1, buzz, "How to learn coding")
-art3=Article.new(author1, nation, "Kenyan way" )
-art4=Article.new(author2, people_daily,"Ways of the society")
-art5=Article.new(author2, nation,"Programmers are problem solvers")
+art1=Article.new(kendi, people, "How to lose weight fast")
+art2=Article.new(martha, buzz, "How to learn coding")
+art3=Article.new(kendi, nation, "Kenyan way" )
+art4=Article.new(martha, people_daily,"Ways of the society")
+art5=Article.new(kendi, nation,"Programmers are problem solvers")
 
-pp author1.articles
-pp author2.articles
+pp kendi.articles
+pp martha.articles
 
 
 
