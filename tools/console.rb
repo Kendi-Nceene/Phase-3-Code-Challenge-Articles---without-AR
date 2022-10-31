748475f06d00d842ddb79fbb27af83eddb5d99da
Kendi-Nceene <kendinceene@gmail.com>
2022-10-31 07:55:51 +0300

    Debugging

--- tools/console.rb
+++ tools/console.rb
@@ -5,30 +5,23 @@ def reload
 end
 
 ###  WRITE YOUR TEST CODE HERE ###
-fizzbuzz = Magazine.new("FizzBuzz", "Technology")
-buzz = Magazine.new("Buzz", "Technology")
-buzz2 = Magazine.new("Buzz", "Technology")
-buzz3 = Magazine.new("Buzz", "Technology")
-buzz4 = Magazine.new("Buzz", "Technology")
-buzz5 = Magazine.new("Buzz", "Technology")
-buzz6 = Magazine.new("Buzz", "Technology")
+# Author
+ author_1 = Author.new( "kendi")
+ author_2= Author.new( "martha")
+
+# Magazine
 people = Magazine.new("People", "Entertainment")
-people2 = Magazine.new("People", "Entertainment")
-people3 = Magazine.new("People", "Entertainment")
+buzz = Magazine.new("Buzz", "Technology")
 nation= Magazine.new("Nation", "Politics")
-nation2= Magazine.new("Nation", "Politics")
-nation3= Magazine.new("Nation", "Politics")
-art1=Article.new("Art1", people, "Author1")
-art2=Article.new("Art2", people, "Author2")
-art3=Article.new("Art3", people, "Author3")
-fizzbuzz.add_article("Author1", "Art1")
-fizzbuzz.add_article("Author2", "Art2")
-fizzbuzz.add_article("Author3", "Art3")
-buzz.add_article("Author1", "Art1")
-buzz.add_article("Author2", "Art2")
 people_daily = Magazine.new("People Daily", "Entertainment")
-people_daily.add_article("Author1", "Art1")
-people_daily.add_article("Author2", "sport")
+# Article
+art1=Article.new("Art1", people, "author1")
+art2=Article.new("Art2", people, "author2")
+
+fizzbuzz.add_article("author1", "Art1")
+fizzbuzz.add_article("author2", "Art2")
+pp author_1.articles
+pp author_2.articles
 
 
 ### DO NOT REMOVE THIS
